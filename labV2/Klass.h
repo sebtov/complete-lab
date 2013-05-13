7cd899aee999d0b1cde352df9d4ea34259c502a3
Sebastian <sebastian_holmqvist@hotmail.com>
2013-05-13 09:03:14 +0200

    validation added

---
+++ labV2/Klass.h
@@ -0,0 +1,13 @@
+//
+//  Klass.h
+//  labV2
+//
+//  Created by sebastian holmqvist on 2013-05-09.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Klass : NSObject
+
+@end
---
+++ labV2/Klass.m
@@ -0,0 +1,13 @@
+//
+//  Klass.m
+//  labV2
+//
+//  Created by sebastian holmqvist on 2013-05-09.
+//  Copyright (c) 2013 Jimmy Lidström. All rights reserved.
+//
+
+#import "Klass.h"
+
+@implementation Klass
+
+@end
